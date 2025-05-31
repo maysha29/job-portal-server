46e2a9ac3e87a6badb7b3498268ec6a607883e91
maysha29 <mayshatabassum29@gmail.com>
2025-05-31 14:32:13 +0600

    App.get part is added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require ("express");
 const cors=require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port=process.env.PORT|| 3000;
 
@@ -34,6 +34,13 @@ async function run() {
         const cursor=(await jobsCollection).find();
         const result=await cursor.toArray();
         res.send(result);
+    })
+
+    app.get('/jobs/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await jobsCollection.findOne(query);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
